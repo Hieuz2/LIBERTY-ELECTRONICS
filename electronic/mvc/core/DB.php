5e21714425936774de069b23c2502ef914c4b2eb
TienDung1605 <TienDung160500>
2023-05-16 05:29:52 +0700

    first commit

---
+++ electronic/mvc/core/DB.php
@@ -0,0 +1,56 @@
+<?php
+
+class DB{
+    public $con;
+    protected $host = 'localhost';
+    protected $username = 'root';
+    protected $password = '';
+    protected $database = 'electronic';
+
+    function __construct()
+    {
+        $this->con = mysqli_connect($this->host, $this->username, $this->password);
+        mysqli_select_db($this->con, $this->database);
+        mysqli_set_charset($this->con, 'utf8');
+    }
+
+    public function execute($sql){
+        mysqli_query($this->con, $sql);
+        
+        return mysqli_insert_id($this->con);
+    }
+
+    // public function executeResult($sql, $isSingle = false){
+    //     $result = mysqli_query($this->con, $sql);
+    //     $data = [];
+    //     if($isSingle){
+    //         $data = mysqli_fetch_array($result, 1);
+    //     }else{
+    //         while(($row = mysqli_fetch_array($result, 1)) != null){
+    //             $data[] = $row;
+    //         }
+    //     }
+
+    //     return $data;
+    // }
+    public function executeResult($sql, $isSingle = false) {
+        $result = mysqli_query($this->con, $sql);
+        $data = [];
+    
+        if (!$result) {
+            echo "Error: " . mysqli_error($this->con);
+            return [];
+        }
+    
+        if ($isSingle) {
+            $data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        } else {
+            while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) !== null) {
+                $data[] = $row;
+            }
+        }
+    
+        return $data;
+    }
+    
+}
